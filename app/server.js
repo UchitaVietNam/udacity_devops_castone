7410995e1dc0e61ac5184edf88a9ac9d44ad8933
tuanvu0696 <tuanvu0696@gmail.com>
2023-09-17 14:04:55 +0900

    Final commit

--- app/server.js
+++ app/server.js
@@ -2,15 +2,14 @@
 
 const express = require("express");
 
-// Constants
-const PORT = 8080;
-const HOST = "0.0.0.0";
-
 // App
 const app = express();
 app.get("/", (req, res) => {
-  res.send("Hello World, my name is NGUYEN TUAN VU!")
+  res.send("Hello World, my name is NGUYEN TUAN VU!");
 });
 
+// Constants
+const PORT = 8080;
+const HOST = "0.0.0.0";
 app.listen(PORT, HOST);
 console.log(`Running the server app on http://${HOST}:${PORT}`);
