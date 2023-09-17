19bdfd48f37ed5408f05d01a6eb1f92e18836443
tuanvu0696 <tuanvu0696@gmail.com>
2023-09-17 14:01:49 +0900

    Update screenshots

--- app/server.js
+++ app/server.js
@@ -1,18 +1,16 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 
 // Constants
 const PORT = 8080;
-const HOST = '0.0.0.0';
+const HOST = "0.0.0.0";
 
 // App
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World, my name is NGUYEN TUAN VU!');
+app.get("/", (req, res) => {
+  res.send("Hello World, my name is NGUYEN TUAN VU!")
 });
 
 app.listen(PORT, HOST);
 console.log(`Running the server app on http://${HOST}:${PORT}`);
-
-
